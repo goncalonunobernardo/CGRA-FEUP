8168e2ad26a2e98ecd25a3ac9215890f51315e94
Simawatt <simaoxo@hotmail.com>
2018-05-23 15:12:42 +0100

    Added textures to crane, missing only defining areas

--- WORKSPACE/TRAB6/tp6/MyCraneArm.js
+++ WORKSPACE/TRAB6/tp6/MyCraneArm.js
@@ -10,10 +10,35 @@ class MyCraneArm extends CGFobject
   {
     super(scene);
 
-    this.materialDefault = new CGFappearance(this.scene);
+    this.armAppearance = new CGFappearance(this.scene);
+    this.armAppearance.setAmbient(0.8, 0.8, 0.8, 1);
+		this.armAppearance.setDiffuse(0.5, 0.5, 0.5, 1);
+		this.armAppearance.setSpecular(0.5, 0.5, 0.5, 1);
+		this.armAppearance.setShininess(25);
+    this.armAppearance.loadTexture("../resources/images/C_arms.png");
 
-    this.base = new MyToppedCylinder(scene, 30, 3, this.materialDefault, this.materialDefault);
-    this.arm = new MyToppedCylinder(scene, 30, 10, this.materialDefault, this.materialDefault);
+    this.postAppearance = new CGFappearance(this.scene);
+    this.postAppearance.setAmbient(0.8, 0.8, 0.8, 1);
+		this.postAppearance.setDiffuse(0.5, 0.5, 0.5, 1);
+		this.postAppearance.setSpecular(0.5, 0.5, 0.5, 1);
+		this.postAppearance.setShininess(100);
+    this.postAppearance.loadTexture("../resources/images/C_post.png");
+
+    this.magnetSide = new CGFappearance(this.scene);
+    this.magnetSide.setDiffuse(0.5, 0.5, 0.5, 1);
+    this.magnetSide.setSpecular(0.5, 0.5, 0.5, 1);
+    this.magnetSide.setShininess(25);
+    this.magnetSide.loadTexture("../resources/images/C_magnetSide.png");
+
+    this.magnetTop = new CGFappearance(this.scene);
+    this.magnetTop.setAmbient(0.8, 0.8, 0.8, 1);
+    this.magnetTop.setDiffuse(0.5, 0.5, 0.5, 1);
+    this.magnetTop.setShininess(25);
+    this.magnetTop.loadTexture("../resources/images/C_magnetTop.png");
+
+    this.arm = new MyToppedCylinder(scene, 30, 10, this.armAppearance, this.armAppearance);
+    this.post = new MyToppedCylinder(scene, 30, 10, this.postAppearance, this.postAppearance);
+    this.magnet = new MyToppedCylinder(scene, 30, 3, this.magnetSide, this.magnetTop);
 
     this.angD = -Math.PI/50;
     this.xD = 2.3;
@@ -45,14 +70,14 @@ class MyCraneArm extends CGFobject
       this.scene.translate(this.xD, this.yD, 0);
       this.scene.scale(0.1, 1.5, 0.1);
       this.scene.rotate(-Math.PI/2, 1, 0, 0);
-      this.arm.display();
+      this.post.display();
     this.scene.popMatrix();
 
     this.scene.pushMatrix();
       this.scene.translate(this.xD, this.yD, 0);
       this.scene.scale(0.8, 0.1, 0.8);
       this.scene.rotate(-Math.PI/2, 1, 0, 0);
-      this.base.display();
+      this.magnet.display();
     this.scene.popMatrix();
 
   };
--- WORKSPACE/TRAB6/tp6/MyCraneBase.js
+++ WORKSPACE/TRAB6/tp6/MyCraneBase.js
@@ -10,12 +10,41 @@ class MyCraneBase extends CGFobject
   {
     super(scene);
 
-    this.materialDefault = new CGFappearance(this.scene);
+    this.baseSide = new CGFappearance(this.scene);
+    this.baseSide.setDiffuse(0.5, 0.5, 0.5, 1);
+    this.baseSide.setSpecular(0.5, 0.5, 0.5, 1);
+    this.baseSide.setShininess(15);
+    this.baseSide.loadTexture("../resources/images/C_baseSide.png");
+
+    this.baseTop = new CGFappearance(this.scene);
+    this.baseTop.setAmbient(0.6, 0.6, 0.6, 1);
+    this.baseTop.setDiffuse(0.5, 0.5, 0.5, 1);
+    this.baseTop.setSpecular(0.5, 0.5, 0.5, 1);
+    this.baseTop.setShininess(15);
+    this.baseTop.loadTexture("../resources/images/C_baseTop.png");
+
+    this.armAppearance = new CGFappearance(this.scene);
+    this.armAppearance.setAmbient(0.8, 0.8, 0.8, 1);
+		this.armAppearance.setDiffuse(0.5, 0.5, 0.5, 1);
+		this.armAppearance.setSpecular(0.5, 0.5, 0.5, 1);
+		this.armAppearance.setShininess(25);
+    this.armAppearance.loadTexture("../resources/images/C_arms.png");
+
+    this.axisSide = new CGFappearance(this.scene);
+    this.axisSide.setDiffuse(0.5, 0.5, 0.5, 1);
+    this.axisSide.setShininess(15);
+    this.axisSide.loadTexture("../resources/images/C_axisSide.png");
+
+    this.axisTop = new CGFappearance(this.scene);
+    this.axisTop.setDiffuse(0.5, 0.5, 0.5, 1);
+    this.axisTop.setShininess(15);
+    this.axisTop.loadTexture("../resources/images/C_axisTop.png");
+
+    this.base = new MyToppedCylinder(scene, 30, 3, this.baseSide, this.baseTop);
+    this.arm = new MyToppedCylinder(scene, 30, 10, this.armAppearance, this.armAppearance);
+    this.axis = new MyToppedCylinder(scene, 30, 3, this.axisSide, this.axisTop);
 
     this.craneArm = new MyCraneArm(scene);
-
-    this.base = new MyToppedCylinder(scene, 30, 3, this.materialDefault, this.materialDefault);
-    this.arm = new MyToppedCylinder(scene, 30, 10, this.materialDefault, this.materialDefault);
 
     this.angB= Math.PI;
 
@@ -60,7 +89,7 @@ class MyCraneBase extends CGFobject
     this.scene.pushMatrix();
       this.scene.translate(2.4, 5.9, -0.35);
       this.scene.scale(0.65, 0.65, 0.7);
-      this.base.display();
+      this.axis.display();
     this.scene.popMatrix();
 
     this.craneArm.display();
